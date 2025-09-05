58bd39c6313e9fa75f18a6daaaa99690ea9a8a1a
Nabin Karki <karki0008@gmail.com>
2025-09-05 21:55:53 +0545

    Changes made on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,10 @@
-import React, { use, useContext, useEffect, useState } from "react";
+import React, {
+  use,
+  useContext,
+  useEffect,
+  useState,
+  useSyncExternalStore,
+} from "react";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/latestCollection";
 import BestSeller from "../components/BestSeller";
@@ -7,39 +13,92 @@ import NewsletterBox from "../components/NewsletterBox";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { ShopContext } from "../context/ShopContext";
 import ProductItem from "../components/Productitem";
+import Title from "../components/Title";
 const Home = () => {
   const { products } = useContext(ShopContext);
-  const [hide, setHide] = useState(false);
+  const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [sortType, setSortType] = useState("relavant");
+  const toggleFunction = (e) => {
+    if (category.includes(e.target.value)) {
+      setCategory((prev) => prev.filter((item) => item !== e.target.value));
+    } else {
+      setCategory((prev) => [...prev, e.target.value]);
+    }
+  };
+
+  const applyFilter = () => {
+    let filterCopy = products.slice();
+    if (category.length > 0) {
+      filterCopy = filterCopy.filter((item) =>
+        category.includes(item.category)
+      );
+    }
+    setFilterProducts(filterCopy);
+  };
+
+  const sortingFunction = (e) => {
+    const sortCopy = filterProducts.slice();
+    switch (sortType) {
+      case "low-high":
+        setFilterProducts(sortCopy.sort((a, b) => a.price - b.price));
+        break;
+      case "high-low":
+        setFilterProducts(sortCopy.sort((a, b) => b.price - a.price));
+        break;
+      default:
+        applyFilter();
+        break;
+    }
+  };
+  useEffect(() => {
+    sortingFunction();
+  }, [sortType]);
   useEffect(() => {
     setFilterProducts(products);
-  });
+  }, []);
+  useEffect(() => {
+    applyFilter();
+  }, [category]);
+
   return (
     <div>
-      <div>
-        <p onClick={() => setHide(!hide)}>Filter</p>
-        <div className={`border w-25 h-20 ${hide ? "" : "hidden"} sm:block  `}>
-          CATEGORY
-          <p className="flex gap">
-            <input type="checkbox" value="MALE" />
-            MALE
+      <div onClick={() => setShowFilter(!showFilter)}>
+        Filters
+        <div
+          className={`border w-20 h-20 ${showFilter ? "" : "hidden"} sm:block`}
+        >
+          Categories
+          <p className="flex gap-2">
+            <input type="checkbox" onChange={toggleFunction} value="Men" />
+            MEn
           </p>
-          <p className="flex gap">
-            <input type="checkbox" value="FEMALE" />
-            FEMALE
+          <p className="flex gap-2">
+            <input type="checkbox" onChange={toggleFunction} value="Women" />
+            WOmen
           </p>
         </div>
-      </div>
-      <div>
-        {filterProducts.map((item, index) => (
-          <ProductItem
-            key={index}
-            id={item._id}
-            name={item.name}
-            price={item.price}
-            image={item.image}
-          />
-        ))}
+        <div>
+          <select
+            onChange={(e) => setSortType(e.target.value)}
+            className="border-2 border-gray-300 text-sm px-2"
+          >
+            <option value="relavent">Sort by:Relavant</option>
+            <option value="low-high">Sort by:Low to High</option>
+            <option value="high-low">Sort by:High to Low</option>
+          </select>
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
+            {filterProducts.map((item, index) => (
+              <ProductItem
+                key={index}
+                name={item.name}
+                price={item.price}
+                image={item.image}
+              />
+            ))}
+          </div>
+        </div>
       </div>
       <Hero />
       <LatestCollection />
