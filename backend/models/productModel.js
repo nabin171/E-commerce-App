f3d5d0f81d3a02d4cc6f1e22225b357f10b0bb8b
Nabin Karki <karki0008@gmail.com>
2025-09-09 21:21:57 +0545

    backend folder structure has been created

---
+++ backend/models/productModel.js
@@ -0,0 +1,40 @@
+import mongoose, { mongo } from "mongoose";
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: Array,
+    requird: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  subcategory: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: Array,
+    required: true,
+  },
+  bestSeller: {
+    type: Boolean,
+  },
+  Date: { type: Number, required: true },
+});
+
+const productModel =
+  mongoose.models.product || mongoose.model("product", productSchema);
+
+export default productModel;
